32f1fe112730ffcb889b34a71c51666dc65add01
Michael Wieland <git@selfcoders.com>
2023-06-08 13:58:46 +0200

    Do not check characters in path, query string and fragment

--- background.js
+++ background.js
@@ -1,18 +1,38 @@
-function checkUrl(requestDetails) {
-    let url = decodeURI(requestDetails.url);
+function isBadUrl(url) {
+    let parsedUrl = new URL(url);
+
+    // Remove path, query string and fragment, so they are not checked for bad characters
+    parsedUrl.pathname = "";
+    parsedUrl.search = "";
+    parsedUrl.hash = "";
+
+    // New URL only containing the base URL with potential bad characters
+    url = decodeURI(parsedUrl.toString());
 
     for (let index = 0; index < url.length; index++) {
         let charCode = url.charCodeAt(index);
 
+        // Check if this character is outside the allowed character range
         if (charCode < 32 || charCode > 255) {
-            browser.browserAction.setBadgeBackgroundColor({"color": "red"});
-            browser.browserAction.setBadgeText({text: "BLK"});
-            browser.browserAction.setTitle({title: `Request Blocked: ${url}`});
-            console.log(`Request Blocked: ${url}`);
-            return {
-                cancel: true
-            };
+            return true;
         }
+    }
+
+    return false;
+}
+
+function onBeforeRequest(requestDetails) {
+    let url = requestDetails.url;
+
+    if (isBadUrl(url)) {
+        browser.browserAction.setBadgeBackgroundColor({"color": "red"});
+        browser.browserAction.setBadgeText({text: "BLK"});
+        browser.browserAction.setTitle({title: `Request Blocked: ${url}`});
+        console.log(`Request Blocked: ${url}`);
+
+        return {
+            cancel: true
+        };
     }
 
     browser.browserAction.setBadgeBackgroundColor({"color": "green"});
@@ -21,7 +41,7 @@ function checkUrl(requestDetails) {
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-    checkUrl,
+    onBeforeRequest,
     {urls: ["<all_urls>"]},
     ["blocking"]
 );
